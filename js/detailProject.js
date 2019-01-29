7ac1c74fa14b5bcb992cd81ef3eb0b544c27d767
nestorhdez <nestorhdez.26@gmail.com>
2019-01-29 15:34:43 +0000

    Links of project's sections added.

--- js/detailProject.js
+++ js/detailProject.js
@@ -6,33 +6,37 @@ function filterProject(arrProjects) {
 }
 
 function htmlProjectSection(section, index) {
-    let template = (`
+    return (`
         <div class="text-section${index%2 !== 0 ? ' reverse-text' : ''}">
             ${section.sectionTitle ? `<h2>${section.sectionTitle}</h2>` : ''}
             ${section.sectionSubtitle ? `<span class="section-subtitle">${section.sectionSubtitle}</span>` : ''}
+            ${sectionLinks(section)}
             ${section.sectionTitle || section.sectionSubtitle ? `<p class="section-description">${section.sectionDescription}</p>` : `<p>${section.sectionDescription}</p>`}
         </div>
         <div class="image-section${index%2 !== 0 ? ' reverse-img' : ''}">
             ${section.image ? `<img src="${section.image}" alt="Screenshot image">` : ""}
         </div>
     `);
-    return template;
 }
 
 function htmlProjectSkills(string) {
-    let skillIcon = (`
+    return (`
         <i class="skill-icon fab fa-${string}"></i>
     `);
-    return skillIcon;
 }
 
 function htmlLinksProject(project) {
-    let linkIcon = (`
+    return (`
        ${project.linksToProject.github ?  `<a title="Github" href="${project.linksToProject.github}" target="_blank"><i class="skill-icon fab fa-github"></i></a>` : null}
        ${project.linksToProject.web ?  `<a title="Web site" href="${project.linksToProject.web}" target="_blank"><i class="skill-icon fas fa-link"></i></a>` : null}
     `);
+}
 
-    return linkIcon;
+function sectionLinks(section) {
+    return (`
+        ${section.sectionGithubLink ? `<a href="${section.sectionGithubLink}" target="_blank"><i class="section-link fab fa-github"></i></a>` : ''}
+        ${section.sectionWebLink ? `<a href="${section.sectionWebLink}" target="_blank"><i class="section-link fas fa-link"></i></a>` : ''}
+    `);
 }
 
 function renderHtmlProject(project) {
