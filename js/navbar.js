b9985169ac5bf8544f566f708cf1c6af0d6350fe
Nestor hdez <nestorhdez.26@gmail.com>
2019-11-15 12:22:39 +0000

    Refactor of navbar.

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 //Generate hamburger menu effect
 function hamburgerMenu() {
-    let divClass = document.querySelector('#menu-hamb');
-    let navbar = document.querySelector('#navbar-container');
-    let navbarMobile = document.querySelector('#nav-mobile');
+    const divClass = document.querySelector('#menu-hamb');
+    const navbar = document.querySelector('#navbar-container');
+    const navbarMobile = document.querySelector('#nav-mobile');
     
     if(divClass.className === 'cross') { 
         divClass.className = '';
@@ -16,19 +16,9 @@ function hamburgerMenu() {
     };
 }
 
-window.addEventListener('resize', () => {
-    if(window.innerWidth >= 768){
-        document.querySelector('#nav-mobile').setAttribute('style', 'display: none;');
-        document.querySelector('#menu-hamb').className = '';
-    }
-});
-
-document.querySelector('#menu-hamb').addEventListener('click', hamburgerMenu);
-document.querySelectorAll('.nav-link-mobile').forEach(link => link.addEventListener('click', hamburgerMenu));
-
 // Change navbar color on scroll
 function navbarColor() {
-    let navbar = document.querySelector('#navbar-container');
+    const navbar = document.querySelector('#navbar-container');
     if(document.documentElement.scrollTop > 50){
         navbar.setAttribute('style', 'background-color: #111;')
     }else {
@@ -37,4 +27,22 @@ function navbarColor() {
         }
     }
 }
+
+// Change navbar depends the width of the screen
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 768){
+        document.querySelector('#nav-mobile').setAttribute('style', 'display: none;');
+        document.querySelector('#menu-hamb').className = '';
+    }
+});
+
+// Set the height of the mobile navbar
+const navbarHeight = document.querySelector('#navbar-container').clientHeight;
+const linksContainer = document.querySelector('#nav-mobile');
+const linksHeight = linksContainer.clientHeight;
+const linksMargin = +window.getComputedStyle(linksContainer).getPropertyValue('margin-top').split('px')[0];
+document.documentElement.style.setProperty('--nav-mobile-height', `${navbarHeight + linksHeight + linksMargin}px`);
+
+document.querySelector('#menu-hamb').addEventListener('click', hamburgerMenu);
+document.querySelector('#nav-mobile').addEventListener('click', hamburgerMenu);
 window.onscroll = navbarColor;
