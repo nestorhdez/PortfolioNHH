8e354e1714e9b4c579cd9a40494e1c9e937a96ec
Nestor hdez <nestorhdez.26@gmail.com>
2019-12-04 11:08:59 +0000

    Revert "Refactor of navbar."
    This reverts commit b9985169ac5bf8544f566f708cf1c6af0d6350fe.

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 //Generate hamburger menu effect
 function hamburgerMenu() {
-    const divClass = document.querySelector('#menu-hamb');
-    const navbar = document.querySelector('#navbar-container');
-    const navbarMobile = document.querySelector('#nav-mobile');
+    let divClass = document.querySelector('#menu-hamb');
+    let navbar = document.querySelector('#navbar-container');
+    let navbarMobile = document.querySelector('#nav-mobile');
     
     if(divClass.className === 'cross') { 
         divClass.className = '';
@@ -16,9 +16,19 @@ function hamburgerMenu() {
     };
 }
 
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 768){
+        document.querySelector('#nav-mobile').setAttribute('style', 'display: none;');
+        document.querySelector('#menu-hamb').className = '';
+    }
+});
+
+document.querySelector('#menu-hamb').addEventListener('click', hamburgerMenu);
+document.querySelectorAll('.nav-link-mobile').forEach(link => link.addEventListener('click', hamburgerMenu));
+
 // Change navbar color on scroll
 function navbarColor() {
-    const navbar = document.querySelector('#navbar-container');
+    let navbar = document.querySelector('#navbar-container');
     if(document.documentElement.scrollTop > 50){
         navbar.setAttribute('style', 'background-color: #111;')
     }else {
@@ -35,15 +45,6 @@ window.addEventListener('resize', () => {
         document.querySelector('#menu-hamb').className = '';
     }
 });
-
-// Set the height of the mobile navbar
-setTimeout(() => {
-    const navbarHeight = document.querySelector('#navbar-container').clientHeight;
-    const linksContainer = document.querySelector('#nav-mobile');
-    const linksHeight = linksContainer.clientHeight;
-    const linksMargin = +window.getComputedStyle(linksContainer).getPropertyValue('margin-top').split('px')[0];
-    document.documentElement.style.setProperty('--nav-mobile-height', `${navbarHeight + linksHeight + linksMargin}px`);
-}, 500);
 
 document.querySelector('#menu-hamb').addEventListener('click', hamburgerMenu);
 document.querySelector('#nav-mobile').addEventListener('click', hamburgerMenu);
