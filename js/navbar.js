97c648eb0315ed25297cce3ab04dc25acff8ee9a
Nestor hdez <nestorhdez.26@gmail.com>
2020-01-19 23:08:30 +0000

    Refactor navbar.js and change animations of navbar.

--- js/navbar.js
+++ js/navbar.js
@@ -1,41 +1,25 @@
-//Generate hamburger menu effect
-function hamburgerMenu() {
-    let divClass = document.querySelector('#menu-hamb');
-    let navbar = document.querySelector('#navbar-container');
-    let navbarMobile = document.querySelector('#nav-mobile');
-    
-    if(divClass.className === 'cross') { 
-        divClass.className = '';
-        navbar.className = 'menu-animation-close';
-        document.documentElement.scrollTop < 50 ? navbar.setAttribute('style', 'background-color: transparent') : '';
-    } else {
-        divClass.className = 'cross';
-        navbarMobile.setAttribute('style', 'display: initial;');
-        navbar.className = 'menu-animation-open';
-        document.documentElement.scrollTop < 50 ? navbar.setAttribute('style', 'background-color: #111') : '';
-    };
+const navSelector = {
+    root: document.documentElement,
+    navbar: document.querySelector('#navbar-container'),
+    hamburger: document.querySelector('#menu-hamb'),
+    navMobile: document.querySelector('#nav-mobile')
 }
 
-// Change navbar color on scroll
-function navbarColor() {
-    let navbar = document.querySelector('#navbar-container');
-    if(document.documentElement.scrollTop > 50){
-        navbar.setAttribute('style', 'background-color: #111;')
-    }else {
-        if(document.querySelector('#menu-hamb').className === ''){
-            navbar.setAttribute('style', 'background-color: transparent');
-        }
-    }
+const toggleNavColor = () => {
+    navSelector.root.scrollTop > 100 || navSelector.hamburger.className === 'cross' ? 
+        navSelector.root.style.setProperty('--bg-navbar', '#111') :
+        navSelector.root.style.setProperty('--bg-navbar', 'transparent');
 }
 
-// Change navbar depends the width of the screen
-window.addEventListener('resize', () => {
-    if(window.innerWidth >= 768){
-        document.querySelector('#nav-mobile').setAttribute('style', 'display: none;');
-        document.querySelector('#menu-hamb').className = '';
-    }
-});
+const toggleHamburgerMenu = () => {
+    navSelector.navbar.classList.toggle('open');
+    navSelector.navMobile.classList.toggle('show');
+    navSelector.hamburger.classList.toggle('cross');
+    toggleNavColor();
+}
 
-document.querySelector('#menu-hamb').addEventListener('click', hamburgerMenu);
-document.querySelector('#nav-mobile').addEventListener('click', hamburgerMenu);
-window.onscroll = navbarColor;
+navSelector.hamburger.addEventListener('click', toggleHamburgerMenu);
+navSelector.navMobile.addEventListener('click', toggleHamburgerMenu);
+window.onscroll = toggleNavColor;
+window.onresize = () => window.innerWidth >= 768 && navSelector.hamburger.className === 'cross' ?
+    toggleHamburgerMenu() : '';
