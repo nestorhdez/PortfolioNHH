cf20df4a131e2e3db4076d473fc8db9971fceaa7
Nestor hdez <nestorhdez.26@gmail.com>
2020-01-22 20:41:34 +0000

    Removing nav-mobile/desktop to have just one nav.

--- js/navbar.js
+++ js/navbar.js
@@ -1,25 +1,27 @@
-const navSelector = {
+const { root, navbar, hamburger, linksContainer } = {
     root: document.documentElement,
     navbar: document.querySelector('#navbar-container'),
     hamburger: document.querySelector('#menu-hamb'),
-    navMobile: document.querySelector('#nav-mobile')
+    linksContainer: document.querySelector('#nav-links-container')
 }
 
 const toggleNavColor = () => {
-    navSelector.root.scrollTop > 100 || navSelector.hamburger.className === 'cross' ? 
-        navSelector.root.style.setProperty('--bg-navbar', '#111') :
-        navSelector.root.style.setProperty('--bg-navbar', 'transparent');
+    root.scrollTop > 100 || hamburger.className === 'cross' ? 
+        root.style.setProperty('--bg-navbar', '#111') :
+        root.style.setProperty('--bg-navbar', 'transparent');
 }
 
 const toggleHamburgerMenu = () => {
-    navSelector.navbar.classList.toggle('open');
-    navSelector.navMobile.classList.toggle('show');
-    navSelector.hamburger.classList.toggle('cross');
-    toggleNavColor();
+    if(window.innerWidth < 768 || hamburger.className === 'cross') {
+        navbar.classList.toggle('open');
+        linksContainer.classList.toggle('show');
+        hamburger.classList.toggle('cross');
+        toggleNavColor();
+    }
 }
 
-navSelector.hamburger.addEventListener('click', toggleHamburgerMenu);
-navSelector.navMobile.addEventListener('click', toggleHamburgerMenu);
+hamburger.addEventListener('click', toggleHamburgerMenu);
+linksContainer.addEventListener('click', toggleHamburgerMenu);
 window.onscroll = toggleNavColor;
-window.onresize = () => window.innerWidth >= 768 && navSelector.hamburger.className === 'cross' ?
+window.onresize = () => window.innerWidth >= 768 && hamburger.className === 'cross' ?
     toggleHamburgerMenu() : '';
